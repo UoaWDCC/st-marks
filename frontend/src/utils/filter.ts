abb1155ad8c6b9bc051d37375e840954036ec222
Jordan Wen Jie See <wsee046@aucklanduni.ac.nz>
2022-05-24 01:57:46 +1200

    Add functionality to filter people by death day, month, year or a combination of each

--- frontend/src/utils/filter.ts
+++ frontend/src/utils/filter.ts
@@ -1,4 +1,4 @@
-import { IPerson } from "../types/schema";
+import { IPerson, IDate } from "../types/schema";
 
 export const filterPeopleByFullName = (
   people: IPerson[],
@@ -6,4 +6,14 @@ export const filterPeopleByFullName = (
 ): IPerson[] =>
   people.filter((person) =>
     person.fullName.toLocaleLowerCase().includes(filterTerm.toLocaleLowerCase())
+  );
+
+// currently supports filtering by same month or day or year or a combination of each
+export const filterPeopleByDeathDate = (
+  people: IPerson[],
+  filterDate: IDate
+): IPerson[] =>
+  people.filter((person) =>
+    // eslint-disable-next-line
+    (filterDate.year === undefined || filterDate.year === person.dateOfDeath!.year) && (filterDate.month === undefined || filterDate.month === person.dateOfDeath!.month) && (filterDate.day === undefined || filterDate.day === person.dateOfDeath!.day)
   );
