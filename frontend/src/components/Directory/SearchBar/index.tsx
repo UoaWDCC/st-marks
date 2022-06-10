1868615a31060d2453623e4ed4b9fc3099aaa8ac
Michelle <mwan391@aucklanduni.ac.nz>
2022-06-10 17:54:08 +1200

    Add date-picker to search bar

--- frontend/src/components/Directory/SearchBar/index.tsx
+++ frontend/src/components/Directory/SearchBar/index.tsx
@@ -3,12 +3,52 @@ import { Paper, InputBase, Tooltip } from "@mui/material";
 import { Search as SearchIcon } from "@mui/icons-material";
 import styles from "./SearchBar.module.css";
 
+import TextField from '@mui/material/TextField';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+import Dialog from '@mui/material/Dialog';
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { IDate } from "../../../types/schema";
+import { dateToString } from "../../../utils/dates";
+
+import { getDate, getMonth, getYear } from 'date-fns'
+// import getMonth from 'date-fns/getMonth'
+// import getYear from 'date-fns/getYear'
+
+
+
 interface SearchBarProps {
   onSearchTermChange: (newValue: string) => void;
 }
 
 const SearchBar: React.FC<SearchBarProps> = (props: SearchBarProps) => {
+
+  const dateTheme = createTheme({
+    palette: {
+      // mode: "dark"
+      // background: {
+      //   paper: '#e3f2fd'
+      // },
+    },
+  });
+
   const { onSearchTermChange } = props;
+
+  const [date, setDate] = React.useState<IDate | null>(null);
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  date && console.log(date, dateToString(date));
+
 
   return (
     <Paper className={styles.container}>
@@ -22,10 +62,31 @@ const SearchBar: React.FC<SearchBarProps> = (props: SearchBarProps) => {
           className={styles.searchInput}
         />
         <Tooltip title="Search">
-          <SearchIcon />
+          <SearchIcon onClick={handleClickOpen} />
         </Tooltip>
       </div>
-    </Paper>
+      <Dialog open={open} onClose={handleClose}>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <ThemeProvider theme={dateTheme}>
+            <StaticDatePicker
+              className="date-picker"
+              views={['day']}
+              openTo="year"
+              value={date}
+              onChange={(newDateValue: Date | null) => {
+                newDateValue &&
+                  setDate({
+                    year: getYear(newDateValue),
+                    month: getMonth(newDateValue) + 1,
+                    day: getDate(newDateValue)
+                  });
+              }}
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </ThemeProvider>
+        </LocalizationProvider>
+      </Dialog>
+    </Paper >
   );
 };
 
