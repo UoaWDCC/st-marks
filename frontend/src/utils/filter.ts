b98c1d5c61dd39e30fabdee99ae92bcbadc44a71
Michelle <mwan391@aucklanduni.ac.nz>
2022-06-10 14:22:29 +1200

    Create function filter people's DoD within the same week of the current/specified year

--- frontend/src/utils/filter.ts
+++ frontend/src/utils/filter.ts
@@ -1,4 +1,6 @@
 import { IPerson, IDate } from "../types/schema";
+import { isSameWeek } from 'date-fns'
+
 
 export const filterPeopleByFullName = (
   people: IPerson[],
@@ -16,3 +18,14 @@ export const filterPeopleByDeathDate = (
   people.filter((person) =>
     (filterDate.year === undefined || (person.dateOfDeath && filterDate.year === person.dateOfDeath.year)) && (filterDate.month === undefined || person.dateOfDeath && (filterDate.month === person.dateOfDeath.month)) && (filterDate.day === undefined || person.dateOfDeath && (filterDate.day === person.dateOfDeath.day))
   );
+
+export const filterWithinWeek = (
+  people: IPerson[],
+  date: Date
+): IPerson[] =>
+  people.filter((person) => {
+    if (person.dateOfDeath?.month && person.dateOfDeath.day) {
+      return isSameWeek(date, new Date(date.getFullYear(), person.dateOfDeath?.month, person.dateOfDeath?.day), { weekStartsOn: 1 });
+    }
+  });
+
