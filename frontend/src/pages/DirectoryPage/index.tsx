4dfa93aeaacbcd97bd2c4c1b15b19420125308a5
Jordan Wen Jie See <wsee046@aucklanduni.ac.nz>
2022-07-23 01:31:08 +1200

    Fixed anniversary feature

--- frontend/src/pages/DirectoryPage/index.tsx
+++ frontend/src/pages/DirectoryPage/index.tsx
@@ -33,7 +33,7 @@ const DirectoryPage: React.FC = () => {
 
   useEffect(() => {
     people &&
-      setAnniversaryPeople(filterWithinWeek(people, new Date(2022, 6, 12)));
+      setAnniversaryPeople(filterWithinWeek(people, new Date()));
   }, [people]);
 
   const handleClick = (id: string): void => {
